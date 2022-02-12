8f754b9569c8f6a45666d1665bb254d4b8a2eb7f
ZhanhuiLi <shutian.lzh@alibaba-inc.com>
2022-02-13 00:59:26 +0800

    WIP: implement send

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using org.apache.rocketmq;
+using Grpc.Net.Client;
+using apache.rocketmq.v1;
 namespace tests
 {
     [TestClass]
@@ -8,6 +10,13 @@ namespace tests
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void TestRpcClientImplCtor() {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new MessagingService.MessagingServiceClient(channel);
+            RpcClientImpl impl = new RpcClientImpl(client);
         }
     }
 }
