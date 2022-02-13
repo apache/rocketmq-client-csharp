3adaaeb1a8441f9ee14f235e261c25dde27b7266
Li Zhanhui <lizhanhui@gmail.com>
2022-02-13 22:48:55 +0800

    Get familiar to ThreadPool and Task in C#

--- examples/Program.cs
+++ examples/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace examples
 {
+
+    class Foo {
+        public int bar = 1;
+    }
     class Program
     {
+
+        static void RT(Action action, int seconds, CancellationToken token) {
+            if (null == action) {
+                return;
+            }
+
+            Task.Run(async () =>
+            {
+                while(!token.IsCancellationRequested) {
+                    action();
+                    await Task.Delay(TimeSpan.FromSeconds(seconds), token);
+                }
+            });
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int workerThreads;
+            int completionPortThreads;
+            ThreadPool.GetMaxThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"Max: workerThread={workerThreads}, completionPortThreads={completionPortThreads}");
+            ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"Min: workerThread={workerThreads}, completionPortThreads={completionPortThreads}");
+
+            ThreadPool.QueueUserWorkItem((Object stateInfo) =>
+            {
+                Console.WriteLine("From ThreadPool");
+                if (stateInfo is Foo) {
+                    Console.WriteLine("Foo: bar=" + (stateInfo as Foo).bar);
+                }
+            }, new Foo());
+
+            var cts = new CancellationTokenSource();
+            RT(() =>
+            {
+                Console.WriteLine("Hello Again" + Thread.CurrentThread.Name);
+            }, 1, cts.Token);
+            cts.CancelAfter(3000);
+            Console.ReadKey();
         }
     }
 }
